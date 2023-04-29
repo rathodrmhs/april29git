03ca6134e1bdf515a0ac6b72885942532d39f571
rathodrmhs <108187890+rathodrmhs@users.noreply.github.com>
2023-04-29 12:10:46 -0500

    netbean initial project

---
+++ april29calculator/src/main/java/com/mycompany/april29calculator/Calculator.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.april29calculator;
+
+/**
+ *
+ * @author ratho
+ */
+public class Calculator {
+    
+    public int add(int x, int y){
+        return x+y;
+    }
+    
+        public int mul(int x, int y){
+        return x*y;
+    }
+        
+        
+    
+}
